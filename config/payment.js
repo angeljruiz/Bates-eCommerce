974705cb9250df3bd653a4ec7921b01a7f0fe592
Angel Ruiz-Bates <angelbates5@yahoo.com>
2020-08-23 01:55:06 -0400

    minor changes

--- config/payment.js
+++ config/payment.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const router = express.Router();
-const stripe = require("stripe")(process.env.REACT_APP_STRIPE_SECRET);
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 router.post("/", async (req, res) => {
   let amount = req.body.reduce((sum, item) => sum + Number(item.price), 0);
